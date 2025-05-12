adda129e57e2801390d4a5dbec77a46d84b2dfdf
Sunro1994 <gwangjulsr@gmail.com>
2025-05-12 20:24:16 +0900

    [KAN-2] refactor(user): 회원가입, 로그인 DTO 수정
    - Validation message 추가

--- auth-system/src/main/java/com/intern/authsystem/presentation/dto/request/SignInRequest.java
+++ auth-system/src/main/java/com/intern/authsystem/presentation/dto/request/SignInRequest.java
@@ -1,10 +1,11 @@
 package com.intern.authsystem.presentation.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record SignInRequest(
-        @NotBlank(message = "이메일은 필수 값입니다.") String email,
+        @NotBlank(message = "이메일은 필수 값입니다.") @Email(message = "유효한 이메일 형식이 아닙니다.") String email,
         @NotBlank(message = "비밀번호는 필수값입니다.") @Length(min = 6,message = "비밀번호는 최소 6자리 이상입니다") String password
 ) {
 }
--- auth-system/src/main/java/com/intern/authsystem/presentation/dto/request/SignUpRequest.java
+++ auth-system/src/main/java/com/intern/authsystem/presentation/dto/request/SignUpRequest.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record SignUpRequest(
-        @NotBlank @Email String username,
-        @NotBlank @Length(min = 6) String password,
-        @NotBlank  String nickname
+        @NotBlank(message = "이메일은 필수 값입니다.") @Email(message = "유효한 이메일 형식이 아닙니다.") String email,
+        @NotBlank(message = "비밀번호는 필수 값입니다.") @Length(min = 6, message = "비밀번호는 최소 6자리 이상입니다.") String password,
+        @NotBlank(message = "닉네임은 필수 값입니다.")  String nickname
 ) {
 }
