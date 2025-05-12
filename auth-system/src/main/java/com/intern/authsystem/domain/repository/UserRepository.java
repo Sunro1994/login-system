5bd69a88daf938e569cbd9b4391e5231b975919d
Sunro1994 <gwangjulsr@gmail.com>
2025-05-12 22:29:24 +0900

    [KAN-3] feat(user): Repository 조회, 저장, 검증 추가

--- auth-system/src/main/java/com/intern/authsystem/domain/repository/UserRepository.java
+++ auth-system/src/main/java/com/intern/authsystem/domain/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package com.intern.authsystem.domain.repository;
 
+import com.intern.authsystem.domain.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Optional;
+
 public interface UserRepository {
+    boolean existsByUsername(@NotBlank(message = "이메일은 필수 값입니다.") @Email(message = "유효한 이메일 형식이 아닙니다.") String email);
+
+    User save(User user);
+
+    Optional<User> findByUsername(@NotBlank(message = "이메일은 필수 값입니다.") @Email(message = "유효한 이메일 형식이 아닙니다.") String email);
+
+    Optional<User> findById(Long userId);
 }
