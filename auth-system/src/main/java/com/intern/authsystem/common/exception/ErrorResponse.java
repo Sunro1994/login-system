1cb01a0b62d2eee23c99be67eeaaf394f72dc04d
Sunro1994 <gwangjulsr@gmail.com>
2025-05-12 22:25:26 +0900

    [KAN-3] bug(user): 잘못된 Builder어노테이션 제거
    - 재귀호출에 걸리는 문제 발생

--- auth-system/src/main/java/com/intern/authsystem/common/exception/ErrorResponse.java
+++ auth-system/src/main/java/com/intern/authsystem/common/exception/ErrorResponse.java
@@ -1,12 +1,9 @@
 package com.intern.authsystem.common.exception;
 
-import lombok.Builder;
-
 public record ErrorResponse(ErrorDetail error) {
 
-    @Builder
     public static ErrorResponse of(String code, String message) {
-        return ErrorResponse.builder().code(code).message(message).build();
+        return new ErrorResponse(new ErrorDetail(code, message));
     }
 
     public record ErrorDetail(String code, String message) {}
